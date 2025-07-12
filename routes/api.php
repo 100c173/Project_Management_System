172c4833280172cff60db23b90d22181e30ac397
Amer Oniza <ameroniza@gmail.com>
2025-07-12 17:31:37 +0300

    add api route v1 for authentication

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function(){
+
+    /**
+     ** Authentication User Login/Register/Logout/Me
+     */
+    Route::post('/register',[AuthenticationController::class,'register'])->name('auth.register');
+    Route::post('/login',[AuthenticationController::class,'login'])->middleware('throttle:5,1')->name('auth.login'); // 5 attempts per minute
+    
+
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::post('/logout',[AuthenticationController::class,'logout'])->name('auth.logout');
+        Route::get('/user',[AuthenticationController::class,'user'])->name('auth.me');
+    });
+
+    /**
+     * *****************************************************
+     */
+
+});
